5942d2ec9b55b8786bdb76f3279bbba59d188f0e
ZacWarner <zektgn@gmail.com>
2019-06-23 17:12:39 -0700

    created orm

---
+++ config/orm.js
@@ -0,0 +1,36 @@
+var connection = require("../config/connection.js");
+
+var orm = {
+    selectAll: function (table, cb) {
+        var query = "SELECT * FROM " + table + ";";
+        connection.query(query, function (err, res) {
+            if (err) {
+                throw err;
+            }
+            cb(res);
+        });
+    },
+
+    insertOnce: function (table, name, cb) {
+        var query = "INSERT INTO " + table + " (burger_name) VALUES (" + name + ");";
+        connection.query(query, function (err, res) {
+            if (err) {
+                throw err;
+            }
+            cb(res);
+        });
+
+    },
+
+    updateOne: function (table, id, condition, cb) {
+        var query = "UPDATE " + table + " SET devoured = " + condition + "where id = " + id + ";"
+        connection.query(query, function (err, res) {
+            if (err) {
+                throw err;
+            }
+            cb(res);
+        });
+    }
+};
+
+module.exports = orm;
